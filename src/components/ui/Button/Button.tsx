50555604d4de78e7b3a1c272f3c18aeae9167b4e
elizaveetaPopova <eli.bychkova@gmail.com>
2025-06-02 15:13:43 +0300

    added folder divider in components

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -2,17 +2,27 @@ import styles from './Button.module.scss';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary';
+  size?: 'default' | 'wide';
+  withBorder?: boolean;
   children: React.ReactNode;
   className: string;
 }
 
 const Button = ({
   variant = 'primary',
+  size = 'default',
+  withBorder,
   children,
   className = '',
   ...props
 }: ButtonProps) => {
-  const buttonClass = [styles.button, styles[`button--${variant}`], className]
+  const buttonClass = [
+    styles.button,
+    styles[`button--${variant}`],
+    styles[`button--${size}`],
+    withBorder ? styles['button--with-border'] : styles['button--no-border'],
+    className,
+  ]
     .filter(Boolean)
     .join(' ');
 
